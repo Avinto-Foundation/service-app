6c224a1a057eaa14b51188ba9420a30bfdf36e90
Sabina Giri <girisabina430@gmail.com>
2026-08-13 23:15:07 +0545

    Feat: Add SearchBar, Filter, and Card UI components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,19 @@
 import type { Service } from "../type";
+import {useNavigate} from "react-router-dom";
 
 interface CardProps { 
   service: Service; 
 }
 
 export default function Card({ service }: CardProps) {
-  const { imageUrl, name, category, rating, reviewCount, distanceMiles, price, email } = service;
+  const { imageUrl, name, category, rating, reviewCount, distanceMiles, price } = service;
+const navigate = useNavigate();
 
-  const handleContact = () => 
-    email ? window.location.href = `mailto:${email}` : alert(`Contacting ${name}...`);
+const handleViewProfile = () => {
+  // user clicks "View Profile" button, navigate to the profile page and pass the service data as state
+  navigate(`/profile/${service.id}`, { state: { service } });
+}
+  
 
   return (
     <article className="card">
@@ -16,15 +21,19 @@ export default function Card({ service }: CardProps) {
       <div className="card-body">
         <span className="badge">{category}</span>
         <h2 className="card-title">{name}</h2>
-        <p className="card-rating">
-          <span style={{ color: "#eab308" }}>★</span> <b>{rating}</b> ({reviewCount} reviews)
-        </p>
+      <p className="card-rating">
+  <span style={{ 
+     backgroundColor: "#16a34a",color: "#ffffff",padding: "0.2rem 0.5rem",borderRadius: "0.375rem",fontWeight: "700",display: "inline-flex",    alignItems: "center",    gap: "0.2rem" }}>
+    <span style={{ color: "#facc15" }}>★</span> {rating}
+  </span>
+  <span style={{ marginLeft: "0.5rem",color: "#6b7280"  }}>({reviewCount} reviews)</span>
+</p>
         <div className="card-info">
           <span className="text-muted">{distanceMiles} mi away</span>
-          <b className="text-bold">${price}</b>
+          <b className="text-bold">${price}/hr</b>
         </div>
-        <button onClick={handleContact} className="contact-btn">
-          Contact
+                      <button onClick={handleViewProfile} className="view-profile-btn">
+          View Profile
         </button>
       </div>
     </article>
